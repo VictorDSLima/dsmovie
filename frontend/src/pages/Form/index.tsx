210fa21e94cd4aff7d80c85e9f4cbcfd2712152c
Victor Dionzio <victordlima002@gmail.com>
2022-03-07 10:52:54 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return(
+        <h1>ola formulario</h1>
+    )
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing() {
+    return(
+        <h1>ola</h1>
+    )
+}
+
+export default Listing;
