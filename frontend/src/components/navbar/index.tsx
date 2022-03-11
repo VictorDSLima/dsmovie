d61b5df374a1fb28f7114f3e414546145f1ec630
VictorDSLima <victordlima002@gmail.com>
2022-03-11 17:25:18 -0300

    Error

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,10 +7,10 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/viniciusbacellar">
+          <a href="https://github.com/VictorDSLima">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/viniciusbacellar</p>
+              <p className="dsmovie-contact-link">/VictorDSLima</p>
             </div>
           </a>
         </div>
